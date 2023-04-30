33cd4e58421372428186f4a4aef6aebd3c7e0d40
Dimitri-Lagares <lagares.dimitri@gmail.com>
2023-04-30 16:03:31 -0500

    first commit

---
+++ src/About.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function About() {
+  return (
+    <>
+      <h1>About Page</h1>
+        <p>
+          Welcome to the About page!
+        </p>
+    </>
+  )
+}
+
+export default About;
